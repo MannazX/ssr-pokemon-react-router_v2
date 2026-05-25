ecb272d2d06dd22aa5ab748ccfca5a9c2e251a42
Magnus Sverdrup <magger254@gmail.com>
2026-05-25 13:48:17 +0200

    Removed Robots tags

--- app/routes/kanto.tsx
+++ app/routes/kanto.tsx
@@ -35,8 +35,7 @@ export function meta() {
         {
             name: "description",
             content: "Browse the full list of 151 Kanto Region Pokémon"
-        },
-        { name: "robots", content: "index" }
+        }
     ];
 }
 
